52a446752e40f6fd952269156278d164b44f1751
AdamKoliweszka <adamkoliweszka@gmail.com>
2019-05-31 20:10:31 +0200

    styling add tea

--- src/AddTeaByEmployee.vue
+++ src/AddTeaByEmployee.vue
@@ -1,49 +1,43 @@
 <template>
   <div id="app">
     <MenuForEmployee/>
-     <div>
-        Nazwa:
-        <input type="text" v-model="tea.name">
-      </div>
-      <div>
-        Opis:
-        <textarea v-model="tea.description"/>
-      </div>
-      <div>
-        Cena sprzedaży:
-        <input type="number" v-model="tea.price_of_selling">
-      </div>
-      <div>
-        Cena dostawy:
-        <input type="number" v-model="tea.price_of_delivery">
-      </div>
-      <div>
-        Gatunek:
-        <select v-model="tea.tea_species" class="combo_EdytowanieGatunkow">
-          <option
-            v-for="TeaSpecies in species"
-            :value="TeaSpecies"
-            v-bind:key="'TeaSpecies' + TeaSpecies.name"
-          >{{TeaSpecies.name}}</option>
-        </select>
-      </div>
-      <div>
-        Kraj pochodzenia:
-        <select v-model="tea.country_of_origin" class="combo_EdytowanieKrajow">
-          <option
-            v-for="country in countries"
-            :value="country"
-            v-bind:key="'country' + country.name"
-          >{{country.name}}</option>
-        </select>
-      </div>
-      <div>
-        <input type="button" @click="addTea" value="Dodawanie">
-      </div>
-    
-    <p v-for="(Comunicat,index) in comunicats" v-bind:key="'AddTeaByEmployee'+ index + Comunicat">
-        {{Comunicat}}
-      </p>
+    <form>
+      <label for="name">Nazwa:</label>
+      <input id="name" type="text" v-model="tea.name">
+
+      <label for="description">Opis:</label>
+      <textarea id="description" v-model="tea.description"/>
+
+      <label for="sellingPrice">Cena sprzedaży:</label>
+      <input id="sellingPrice" type="number" v-model="tea.price_of_selling">
+
+      <label for="sellingProvide">Cena dostawy:</label>
+      <input id="sellingProvide" type="number" v-model="tea.price_of_delivery">
+
+      <label for="species">Gatunek:</label>
+      <select id="species" v-model="tea.tea_species" class="combo_EdytowanieGatunkow">
+        <option
+          v-for="TeaSpecies in species"
+          :value="TeaSpecies"
+          v-bind:key="'TeaSpecies' + TeaSpecies.name"
+        >{{TeaSpecies.name}}</option>
+      </select>
+
+      <label for="country">Kraj pochodzenia:</label>
+      <select id="country" v-model="tea.country_of_origin" class="combo_EdytowanieKrajow">
+        <option
+          v-for="country in countries"
+          :value="country"
+          v-bind:key="'country' + country.name"
+        >{{country.name}}</option>
+      </select>
+
+      <input type="button" @click="addTea" value="Dodawanie">
+    </form>
+    <p
+      v-for="(Comunicat,index) in comunicats"
+      v-bind:key="'AddTeaByEmployee'+ index + Comunicat"
+    >{{Comunicat}}</p>
   </div>
 </template>
 
@@ -82,12 +76,13 @@ export default {
   },
   methods: {
     addTea() {
-      DataAccess.addTea(this.tea).then(response => {
-        this.comunicats = [];
-        this.comunicats.push(response.data);
-        this.clear();
-      })
-      .catch(error => {
+      DataAccess.addTea(this.tea)
+        .then(response => {
+          this.comunicats = [];
+          this.comunicats.push(response.data);
+          this.clear();
+        })
+        .catch(error => {
           this.comunicats = [];
           for (var propName in error.response.data) {
             this.comunicats.push(error.response.data[propName]);
@@ -95,7 +90,7 @@ export default {
         });
     },
     clear() {
-        this.tea = {
+      this.tea = {
         id_tea: 0,
         name: "",
         description: "",
@@ -104,12 +99,21 @@ export default {
         available_quantity: 0,
         tea_species: null,
         country_of_origin: null
-      }
+      };
     }
   }
 };
 </script>
 
 
-<style>
+<style scoped>
+input{
+  width: 100%;
+}
+select{
+  width: 100%;
+}
+textarea{
+  width: 100%;
+}
 </style>
